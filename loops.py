8852d6f39aa02a08b9b334e3bec65d0bda903bef
Brijith <brijith522-dot@gmail.com>
2026-07-13 13:01:27 +0530

    Improved multiplication table program

--- loops.py
+++
@@ -1,4 +0,0 @@
-n=int(input("Enter the number from which it have to be summed"))
-for i in range(1,n):
-        print (1+n)
-    
---
+++ multiplication table.py
@@ -0,0 +1,5 @@
+#MULTIPLICATION TABLE
+Number=int(input("Enter the number:"))
+for i in range(1,11):
+        print (i,"X",Number,"=",i*Number)
+    
