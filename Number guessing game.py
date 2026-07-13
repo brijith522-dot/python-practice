b7be21630e25dbaadeef32df25d59d3f24acc70f
Brijith <brijith522-dot@gmail.com>
2026-07-13 12:44:37 +0530

    Added number guessing game

---
+++ Number guessing game.py
@@ -0,0 +1,5 @@
+number=int(input("Guess the number (1-10):"))
+if number==7:
+        print('Correct$$')
+else:
+        print('Wrong!')
---
+++ loops.py
@@ -0,0 +1,4 @@
+n=int(input("Enter the number from which it have to be summed"))
+for i in range(1,n):
+        print (1+n)
+    
