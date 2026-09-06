0644183f63e25d02dd2714754822a776e872dee4
Brijith <brijith522-dot@gmail.com>
2026-09-06 12:07:15 +0530

    Improved age calculator with validation and formatting

--- python-utility-toolkit/age_calculator.py
+++ python-utility-toolkit/age_calculator.py
@@ -1,12 +1,33 @@
 #Age Calculator
-yearofbirth = int(input("Enter the year of birth: "))
-currentyear = int(input("Enter the current year: "))
-if yearofbirth > 2026:
+from  datetime import date
+current_year =date.today().year
+birth_year= int(input("Enter the year of birth: ")) #getting user input
+if birth_year > current_year:                               #validating birth year
     print("You have entered an invalid year of birth, instead of that please enter a valid year of birth.")
 else:
-    age = currentyear - yearofbirth
-    months=age * 12
-    days=age * 365
-    print("Your age is", age,"years.")
-    print("Approximately", months,"months.")
-    print("Approximately", days,"days.")
+    age = current_year - birth_year                 #Age calculation
+    if age>120:
+        print("Please enter a realistic birth year")
+    else:
+        months=age * 12
+        days=age * 365
+
+
+    print("===========================")            #output generation
+    print("AGE REPORT")
+    print("========================")
+    print("Birth year:", birth_year)
+    print("Current year:",current_year)
+    print("Age:",age,"Years")
+    print("Months:",months)
+    print("Days:",days)
+    if age in range(0, 12):
+        status = "Child"
+    elif age in range(13, 17):
+        status = "Teenager"
+    elif age in range(18,59):
+        status = "Adult"
+    elif age in range(60, 120):
+        status = "Senior Citizen"
+    print("Status:",status)
+    print("===========================")
